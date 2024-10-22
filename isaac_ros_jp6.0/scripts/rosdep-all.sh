610aa8100d3712aff1f727e3b5bcc6a7ba1c321a
SamuelFoo <fooenzesamuel@gmail.com>
2024-10-22 18:19:47 +0800

    update docker configs

--- isaac_ros_jp6.0/scripts/rosdep-all.sh
+++ isaac_ros_jp6.0/scripts/rosdep-all.sh
@@ -9,7 +9,6 @@ fi
 if [ -d /workspaces/drone ]; then 
     cd /workspaces/drone ; 
     echo "rosdep updating for drone....." ; 
-    # TODO: Bug fix intercomm_msgs
     # rosdep install --from-paths src -y --ignore-src ; 
     rosdep install --from-paths src --ignore-src --skip-keys intercomm_msgs -y ;
 fi
