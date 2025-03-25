a8e90606debaaf1f9e0c8c2162601bf51809687c
SamuelFoo <fooenzesamuel@gmail.com>
2025-03-26 00:17:50 +0800

    Add on_entry.sh for auv4_orin

---
+++ environments/auv4_orin/on_entry.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Run on entry instead of in Dockerfile since rosdep updates can still happen after the image is built
+rosdep update
+
+WORKSPACE_DIR=/workspaces/ros2_ws
+if [ -d $WORKSPACE_DIR ]; then 
+    cd $WORKSPACE_DIR ; 
+    echo "rosdep updating..." ; 
+    rosdep install --from-paths src -y --ignore-src ; 
+fi
