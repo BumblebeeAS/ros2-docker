e45e6dcafc233457ea51f51928c2de616899804f
SamuelFoo <fooenzesamuel@gmail.com>
2026-08-15 20:18:55 +0200

    Fix quoting in on_entry.sh for better path handling

--- environments/uav2_sim/on_entry.sh
+++ environments/uav2_sim/on_entry.sh
@@ -16,11 +16,11 @@ UAV2_DESC_URDF_DIR=${UAV2_SIM_DIR}/install/uav2_description/share/uav2_descripti
 ETC_DIR=${UAV2_SIM_DIR}/src/bring-up/etc/uav2_sim
 WORLDS_DIR=${UAV2_SIM_DIR}/src/bb_worlds/worlds
 
-mkdir -p ${PX4_AUTOPILOT_MODEL_DIR}
-gz sdf -p ${UAV2_DESC_URDF_DIR}/uav2.urdf > ${PX4_AUTOPILOT_MODEL_DIR}/model.sdf
-ln -sf $(realpath ${ETC_DIR}/model.config) ${PX4_AUTOPILOT_MODEL_DIR}
-ln -sf $(realpath ${ETC_DIR}/5000_gz_uav2) ${PX4_AUTOPILOT_AIRFRAMES_DIR}
-ln -sf $(realpath ${WORLDS_DIR}/robotx_2024_aruco.sdf) ${PX4_AUTOPILOT_GZ_DIR}/worlds
+mkdir -p "${PX4_AUTOPILOT_MODEL_DIR}"
+gz sdf -p "${UAV2_DESC_URDF_DIR}/uav2.urdf" > "${PX4_AUTOPILOT_MODEL_DIR}/model.sdf"
+ln -sf "$(realpath "${ETC_DIR}/model.config")" "${PX4_AUTOPILOT_MODEL_DIR}"
+ln -sf "$(realpath "${ETC_DIR}/5000_gz_uav2")" "${PX4_AUTOPILOT_AIRFRAMES_DIR}"
+ln -sf "$(realpath "${WORLDS_DIR}/robotx_2024_aruco.sdf")" "${PX4_AUTOPILOT_GZ_DIR}/worlds"
 
 if ! grep -q '5000_gz_uav2' "${PX4_AUTOPILOT_AIRFRAMES_DIR}/CMakeLists.txt"; then
     sed -i '/4021_gz_x500_flow/a \ \t5000_gz_uav2' \
